1aba2125ca1141aae0a8694010d5c8665dcc81fd
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-05-12 20:26:12 -0300

    Projeto Feriado v.0.2

--- Aula09/feriados/feriado/views.py
+++ Aula09/feriados/feriado/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def natal(request):
-    return HttpResponse('<h1>É Natal !</h1>')
+    return render(request, 'natal.html')
 
