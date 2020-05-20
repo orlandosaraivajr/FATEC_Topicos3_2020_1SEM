9f7eaf32d506042ea13775daa3fc1cd373366294
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-05-19 21:51:15 -0300

    Aula 10 - ORM

--- Aula09/feriados/feriado/tests.py
+++ Aula09/feriados/feriado/tests.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from feriado.models import FeriadoModel
 from django.test import TestCase
 
 class NatalTest(TestCase):
@@ -12,3 +14,34 @@ class NatalTest(TestCase):
 
     def test_template_usado(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+            nome=self.feriado,
+            dia=self.dia,
+            mes=self.mes,
+        )
+        self.cadastro.save()
+
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
+
+    def test_dia_feriado(self):
+        dia = self.cadastro.__dict__.get('dia', '')
+        self.assertEqual(dia, self.dia)
+
+    def test_mes_feriado(self):
+        mes = self.cadastro.__dict__.get('mes', '')
+        self.assertEqual(mes, self.mes)
